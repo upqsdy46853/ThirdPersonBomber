d56085f352a64e32b6dbd10739575d7284d72fcb
po_chih <55981023+upqsdy46853@users.noreply.github.com>
2023-12-09 19:12:08 +0800

    Add explosion

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -6,22 +6,63 @@ using Fusion;
 public class bullet : NetworkBehaviour
 {
     [Networked] private TickTimer life {get;set;}
+    [Networked] private TickTimer safe {get;set;}
+
+    List<LagCompensatedHit> hits = new List<LagCompensatedHit>();
+    public LayerMask collisionLayers;
+    public LayerMask playerLayer;
+    public LayerMask groundLayer;
+
+    bool isHit = false;
+
     public void Init(Vector3 startingVelocity){
-        life = TickTimer.CreateFromSeconds(Runner, 5.0f);
-        GetComponent<Rigidbody>().velocity = startingVelocity;
+        life = TickTimer.CreateFromSeconds(Runner, 10.0f);
+        safe = TickTimer.CreateFromSeconds(Runner, 0.1f);
+
+        StartCoroutine(spawnedCO(startingVelocity));
+        //GetComponent<Rigidbody>().velocity = startingVelocity;
     }
 
     public override void FixedUpdateNetwork() {
         if(life.Expired(Runner))
             Runner.Despawn(Object);
-    }
-
-    private void OnCollisionEnter(Collision other) {
-        if (Object.HasStateAuthority)
-        {
-            if(other.gameObject.TryGetComponent<PlayerState>(out var state)){
-                state.OnTakeDamage();
+        if(safe.Expired(Runner) && !isHit){
+            int hitCount = Runner.LagCompensation.OverlapSphere(transform.position, 0.5f, Object.InputAuthority, hits, collisionLayers, HitOptions.IncludePhysX);
+            if (hitCount > 0){
+                StartCoroutine(explodeCO());
             }
         }
+    }
+
+    // private void OnCollisionEnter(Collision other) {
+    //     if (Object.HasStateAuthority)
+    //     {
+    //         if(other.gameObject.TryGetComponent<PlayerState>(out var state)){
+    //             state.OnTakeDamage();
+    //         }
+    //     }
+    // }
+
+    IEnumerator spawnedCO(Vector3  startingVelocity)
+    {
+        yield return new WaitForSeconds(0.01f);
+        GetComponent<Rigidbody>().velocity = startingVelocity;
+    }
+
+    IEnumerator explodeCO(){
+        isHit = true;
+        yield return new WaitForSeconds(1f);
+        int hitCount = Runner.LagCompensation.OverlapSphere(transform.position, 30f, Object.InputAuthority, hits, playerLayer, HitOptions.None);
+        for(int i = 0; i<hitCount; i++){
+            var obj = hits[i].Hitbox.Root;
+            if(obj.TryGetComponent<PlayerState>(out var state)){
+                Vector3 direction = obj.transform.position - transform.position;
+                float distance = direction.magnitude;
+                bool blocked = Physics.Raycast(transform.position, direction.normalized, distance, groundLayer);
+                if(!blocked && Object.HasStateAuthority)
+                    state.OnTakeDamage();
+            }
+        }
+        Runner.Despawn(Object);
     }
 }
