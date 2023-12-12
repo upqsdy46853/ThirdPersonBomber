766be18f9c27674d11b6695043cf18d5ed68e54c
Goosiping <georgekuo100@gmail.com>
2023-12-12 22:03:41 +0800

    Fix naming bug

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -18,7 +18,14 @@ public class MenuUIHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // PlayerPrefs.SetString("PlayerNickname", inputField.text);
+        // PlayerPrefs.Save();
+    }
+
+    public void onJoinGameClicked()
+    {
         PlayerPrefs.SetString("PlayerNickname", inputField.text);
         PlayerPrefs.Save();
     }
+
 }
